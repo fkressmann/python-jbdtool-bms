6eeb7c129d5d31bfd426b3b43b5164f648433c03
Felix Kressmann <felix.kressmann@boehringer-ingelheim.com>
2020-08-03 22:25:22 +0200

    Moved querying of data into callable methods which change the BMS object

--- python-jbdtool-bms/BMS.py
+++ python-jbdtool-bms/BMS.py
@@ -30,20 +30,20 @@ def value_to_date(value):
     return datetime(2000 + (value >> 9), (value >> 5) & 0x0f, value & 0x1f)
 
 
-# Can be set if no BMS is connected
+# Can be used if no BMS is connected. Supplies some data captured from my BMS
 def debug_query(query):
     if query == basic_info_query:
         return b'\xdd\x03\x00\x1b\x05\x4c\x00\x00\x24\xf2\x2a\xf8\x00\x00\x28\xd2\x00\x00\x00\x00\x00\x00\x17\x56\x03\x04\x02\x09\x7f\x0b\xa9\xfa\xb0\x77'
     elif query == cell_voltages_query:
         return b'\xdd\x04\x00\x08\x0d\x42\x0d\x3c\x0d\x40\x0d\x3a\xfe\xcc\x77'
     else:
-        return b''
+        raise ValueError("Invalid query")
 
 
 def check_response(query, response):
     # The queried register address needs to match retuned register address and the third byte needs to be 0 for the query to be successful
     if query[2] != response[1] or response[2] != 0:
-        raise ValueError("Response has error")
+        raise ValueError("BMS sent error")
 
     if not validate_response(response):
         raise ValueError("Checksum mismatch")
@@ -51,18 +51,28 @@ def check_response(query, response):
 
 class BMS:
     def __init__(self, serial_port, query_retries=3, offline=False):
-        self.debug = offline
-        self.query_reties = query_retries
+        self.__debug = offline
+        self.__query_reties = query_retries
         if not offline:
             self.connection = serial.Serial(port=serial_port, timeout=1, baudrate=9600, parity=serial.PARITY_EVEN,
                                             stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 
-        self.basic_info_struct_format, self.cell_voltages_struct_format = self.__init_bms()
+        self.__basic_info_struct_format, self.__cell_voltages_struct_format = self.__init_bms()
 
-        # ToDo: Move to method
-        self.voltage, self.current, self.residual_capacity, self.nominal_capacity, self.cycle_times, self.date,\
-            self.rsoc, self.discharge_status, self.charge_status= self.get_basic_info()
-        self.cell_voltages = self.get_cell_voltages()
+        # Init variables
+        self.total_voltage = -1
+        self.current = -1
+        self.residual_capacity = -1
+        self.nominal_capacity = -1
+        self.cycle_times = -1
+        self.manufacturing_date = datetime.now()
+        self.rsoc = -1
+        self.discharge_status = False
+        self.charge_status = False
+        self.cell_voltages = []
+
+        # Initialize data
+        self.query_all()
 
     def __init_bms(self):
         # Only doing this once as these values will not change, saves some µA :D
@@ -76,7 +86,7 @@ class BMS:
         return basic_info_boilerplate + number_of_ntcs * "H", cell_voltages_boilerplate + number_of_cells * "H"
 
     def __query_bms(self, query):
-        if self.debug:
+        if self.__debug:
             return debug_query(query)
         response = bytearray()
         retries = 0
@@ -86,37 +96,39 @@ class BMS:
             self.connection.write(query)
             response.extend(self.connection.read(4))
             retries += 1
-            if retries > self.query_reties:
+            if retries > self.__query_reties:
                 raise ValueError("Cloud not get a response from BMS. Maybe not connected properly?")
 
         # Length of the response is given in byte 3, determines how much bytes to read (+3 for 2 checksum bytes and stop byte)
         length = response[3]
         response.extend(self.connection.read(length + 3))
-
         return response
 
-    def get_basic_info(self):
+    def query_all(self):
+        self.query_basic_info()
+        self.query_cell_voltages()
+
+    def query_basic_info(self):
         response = self.__query_bms(basic_info_query)
         check_response(basic_info_query, response)
-        unpacked = struct.unpack(self.basic_info_struct_format, response[4:-3])
-        data = (
-            unpacked[0] / 100,  # total voltage in 10mV
-            unpacked[1] / 100,  # current in 10mA
-            unpacked[2] / 100,  # residual capacity in 10mAh
-            unpacked[3] / 100,  # nominal capacity in 10mAh
-            unpacked[4],  # cycle times
-            value_to_date(unpacked[5]),  # manufacturing date
-            # 6 balance state low
-            # 7 valance state hight
-            # 8 Protection state
-            # 9 Software version
-            unpacked[10], # remaining state of charge in percent
-            bool(int(bin(unpacked[11])[2])), # discharging status
-            bool(int(bin(unpacked[11])[3])), # charging status
-        )
-        return data
+        unpacked = struct.unpack(self.__basic_info_struct_format, response[4:-3])
+        self.total_voltage = unpacked[0] / 100  # total voltage in 10mV
+        self.current = unpacked[1] / 100  # current in 10mA
+        self.residual_capacity = unpacked[2] / 100  # residual capacity in 10mAh
+        self.nominal_capacity = unpacked[3] / 100  # nominal capacity in 10mAh
+        self.cycle_times = unpacked[4]  # cycle times
+        self.manufacturing_date = value_to_date(unpacked[5])  # manufacturing date
+        # 6 balance state low
+        # 7 valance state hight
+        # 8 Protection state
+        # 9 Software version
+        self.rsoc = unpacked[10] # remaining state of charge in percent
+        self.discharge_status = bool(int(bin(unpacked[11])[2])) # discharging status
+        self.charge_status = bool(int(bin(unpacked[11])[3])) # charging status
 
-    def get_cell_voltages(self):
+    def query_cell_voltages(self):
         response = self.__query_bms(cell_voltages_query)
         check_response(cell_voltages_query, response)
-        return struct.unpack(self.cell_voltages_struct_format, response[4:-3])
+        raw_voltages = struct.unpack(self.__cell_voltages_struct_format, response[4:-3])
+        # Unit is mV, divide by 1000 to get volts
+        self.cell_voltages = [raw / 1000 for raw in raw_voltages]
